4f33d4843e125beb32178d2243a235b927f92afa
Tyuiu.ModenovaAP@outlook.com <fanriall@mail.ru>
2023-11-06 00:36:31 +0500

    Task1

---
+++ Tyuiu.ModenovaAP.Sprint4.Task1.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ModenovaAP.Sprint4.Task1.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ModenovaAP.Sprint4.Task1.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73c54368-3d24-4af7-9da0-d3fdaf83809d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
