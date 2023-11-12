f182ca32ba1007ae9998aba057f015f54a448e3c
Tyuiu.ModenovaAP@outlook.com <fanriall@mail.ru>
2023-11-12 15:02:05 +0500

    Task6

---
+++ Tyuiu.ModenovaAP.Sprint4.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ModenovaAP.Sprint4.Task6.V3.Lib;
+
+namespace Tyuiu.ModenovaAP.Sprint4.Task6.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            string[] array = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+
+
+
+            int count = ds.Calculate(array);
+
+
+            Assert.AreEqual(8, count);
+        }
+    }
+}
---
+++ Tyuiu.ModenovaAP.Sprint4.Task6.V3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ModenovaAP.Sprint4.Task6.V3.Lib;
+
+namespace Tyuiu.ModenovaAP.Sprint4.Task6.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Моденова А. П. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнила: Моденова Анастасия Павловна | ИИПб-23-3                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных [ 'Январь', 'Февраль', 'Март', 'Апрель',    *");
+            Console.WriteLine("* 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']   *");
+            Console.WriteLine("* используя класс Array подсчитайте количество элементов, длина которых   *");
+            Console.WriteLine("* меньше 6.                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string[] array = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+
+            int result = ds.Calculate(array);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество элементов, длина которых меньше 6: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
